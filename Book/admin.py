0555134322edd5d471755a924f65064626571394
Zeaur Rahman <rahmanzea31@gmail.com>
2024-07-11 22:37:50 +0600

    commit change

--- Book/admin.py
+++ Book/admin.py
@@ -9,16 +9,13 @@ admin.site.register(UserAccount)
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'borrowing_price', 'image')
-    # Add other fields you want to display in the list view
+   
 
-    # Optionally, you can add search and filter options:
     search_fields = ['title', 'description']
     list_filter = ['borrowing_price']
 
-    # Optionally, you can add readonly fields if needed:
     readonly_fields = ['image']
 
-    # Optionally, you can add fieldsets to organize fields:
     fieldsets = (
         (None, {
             'fields': ('title', 'description', 'borrowing_price')
